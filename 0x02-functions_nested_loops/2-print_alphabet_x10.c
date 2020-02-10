c8046fcdd0f83ab0d33faa7b778d601ef05417c4
MiguelCF06 <1480@holbertonschool.com>
2020-02-10 17:35:21 +0000

    Function prints AlpX10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,21 @@
 #include "holberton.h"
+/**
+ * print_alphabet_x10 - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 void print_alphabet_x10(void)
 {
 int letra = 'a';
 int tiempo;
-while(tiempo < 10)
+while (tiempo < 10)
 {
-for(letra = 'a' ; letra <= 'z'; letra++)
+for (letra = 'a' ; letra <= 'z'; letra++)
 {
 _putchar(letra);
 }
 _putchar('\n');
 tiempo++;
-}           
+}
 
 }
