61a7485713ce49a0ff9fb1dd32ca19bc7c626d5a
MiguelCF06 <1480@holbertonschool.com>
2020-02-11 13:28:58 +0000

    Absolute Value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,19 @@
 #include "holberton.h"
-
+/**
+ * _abs - absolute value of the code.
+ * @r: integer as an Absolute value
+ * Return: Always 0.
+ */
 
 int _abs(int r)
 {
-  if (r < 0)
-    {
-r = r * -1
-  return(r);
-    }
-  else{
-    return(r)
-      }
+if (r < 0)
+{
+r = r * -1;
+return (r);
+}
+else
+{
+return (r);
+}
 }
