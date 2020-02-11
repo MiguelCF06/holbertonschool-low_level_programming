1a6045fb6f9b3de06ef7adc0430ffad7a5cd1fa0
MiguelCF06 <1480@holbertonschool.com>
2020-02-11 13:20:48 +0000

    Check Lowercase Char

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
  * _islower - check the code for Holberton School students.
- * int c - integer of the code
+ * @c: - integer of the code
  * Return: Always 0.
  */
 int _islower(int c)
