07ddc85bfa849b68c8a2178f859ffe5d77256c3d
MiguelCF06 <1480@holbertonschool.com>
2020-02-13 18:17:07 +0000

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,12 +21,16 @@ int main(void)
 		}
 		else if ((num % 3 != 0) && (num % 5 == 0))
 		{
-			printf("Buzz ");
+			if (num == 100)
+			printf("Buzz");
+			else
+				printf("Buzz ");
 		}
 		else
 		{
 			printf("%i ", num);
 		}
+
 	}
 	printf("\n");
 	return (0);
