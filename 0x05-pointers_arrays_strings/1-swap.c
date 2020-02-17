679e811a7fe9fc41d8dbb881516f172fa741b804
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-02-17 23:29:20 +0000

    Tasks

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
-
+/**
+ * swap_int - Function that swap values of two integers.
+ * @a: Integer as a value.
+ * @b: Integer as a value2.
+ * Return: Always 0.
+ */
 void swap_int(int *a, int *b)
 {
 	int swapp;
