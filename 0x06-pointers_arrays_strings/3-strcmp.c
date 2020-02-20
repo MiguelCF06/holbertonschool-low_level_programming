15e02e1b912e113a9056f3de87c459cf215361f2
MiguelCF06 <1480@holbertonschool.com>
2020-02-20 18:38:45 +0000

    Compares two string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,8 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 > *s2)
-		return ((int)*s1 - *s2);
-	else
-		return ((int)*s1 - *s2);
+	int count;
+
+	for (count = 0; s1[count] != '\0' && s2[count] != '\0'; count++)
+	{
+		if (s1[count] != s2[count])
+			return (s1[count] - s2[count]);
+	}
+	return (s1[count] - s2[count]);
 }
