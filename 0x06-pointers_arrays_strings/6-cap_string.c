ee9bb8dd2b89bdce9c101f7e4e71edfd1b48c6de
MiguelCF06 <1480@holbertonschool.com>
2020-02-20 19:40:13 +0000

    Capitalize words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * cap_string - Capitalize the letters.
+ * @s: String.
+ * Return: S that is the string.
+ */
+char *cap_string(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0' ; count++)
+	{
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '.')
+		{
+			if (s[count] == '.' && s[count + 1] == '\n')
+			count += 2;
+			else
+				count += 1;
+			if (s[count] >= 'a' && s[count] <= 'z')
+			{
+				s[count] = s[count] - 32;
+			}
+		}
+	}
+		return (s);
+}
