0ee7e1211e7a4b36392286c702d26512bdff67c9
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-02-21 00:03:27 +0000

    Encode into rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -3,24 +3,21 @@
 char *rot13(char *s)
 {
 	int countstr;
-	int countal;
-	char arrayrotmin[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
-	char arrayletmin[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	char arrayrotMAY[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
-	char arrayletMAY[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	int countltr;
+	char letter[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (countstr = 0; s[countstr] != '\0'; countstr++)
 	{
-		for(countal = 0; countal < 27; countal++)
+		for (countltr = 0; letter[countltr] != '\0';countltr++)
 		{
-			if(s[countstr] == arrayletmin[countal] && s[countstr] == arrayletMAY[countal])
+			if (s[countstr] == letter[countltr])
 			{
-				s[countstr] = arrayrotmin[countal];
+				s[countstr] = rot[countltr];
+				break;
 			}
-			else
-			{
-				s[countstr] = arrayrotMAY[countal];
-			}
-		}
-	}
+		}	
+	}		
 	return (s);
 }
+	
