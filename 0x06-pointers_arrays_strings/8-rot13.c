f13ecaf5bb5bd44d2e557e040687d106937f2299
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-02-21 00:08:31 +0000

    Encode in rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,5 +1,9 @@
 #include "holberton.h"
-
+/**
+ * rot13 - Encode a string in rot13.
+ * @s: String.
+ * Return: Always 0.
+ */
 char *rot13(char *s)
 {
 	int countstr;
@@ -9,15 +13,14 @@ char *rot13(char *s)
 
 	for (countstr = 0; s[countstr] != '\0'; countstr++)
 	{
-		for (countltr = 0; letter[countltr] != '\0';countltr++)
+		for (countltr = 0; letter[countltr] != '\0'; countltr++)
 		{
 			if (s[countstr] == letter[countltr])
 			{
 				s[countstr] = rot[countltr];
 				break;
 			}
-		}	
-	}		
+		}
+	}
 	return (s);
 }
-	
