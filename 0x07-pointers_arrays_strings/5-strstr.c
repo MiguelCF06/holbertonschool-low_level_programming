04ddc7efa42b3dd8b3d28fbe125d3bae47c99f46
MiguelCF06 <1480@holbertonschool.com>
2020-02-24 18:30:18 +0000

    Fix: Substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  * _strstr - Locates a substring
  * @haystack: String
@@ -22,5 +23,7 @@ char *_strstr(char *haystack, char *needle)
 		}
 		haystack++;
 	}
-	return ('\0');
+	if (*haystack == '\0')
+		return (NULL);
+	return (haystack);
 }
