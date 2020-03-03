b3d6c453d2b19e102acc1a3f82c179b7d18b85aa
MiguelCF06 <1480@holbertonschool.com>
2020-03-03 17:14:01 +0000

    Update: Task 2 Malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - Concatenates a string.
+ * @s1: String 1
+ * @s2: String 2
+ * Return: Null or the array concatenate.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int con1;
+	int con2;
+	char *conar;
+
+	conar = malloc(sizeof(s1) + sizeof(s2));
+	if (conar == NULL)
+		return (0);
+	for (con1 = 0; s1[con1] != '\0'; con1++)
+	{
+		conar[con1] = s1[con1];
+	}
+	for (con2 = 0; s2[con2] != '\0'; con1++, con2++)
+	{
+		conar[con1] = s2[con2];
+	}
+	return (conar);
+}
