79f5c016f594c2869fa2dca0c9318363bb6ce9e2
MiguelCF06 <1480@holbertonschool.com>
2020-03-03 16:09:23 +0000

    Update: Task 1 Malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - Duplicates a string.
+ * @str: String to duplicate
+ * Return: Null or the duplicate.
+ */
+char *_strdup(char *str)
+{
+	int count = 0;
+	int length = 0;
+	char *dup;
+
+	while (str[count] != '\0')
+	{
+		length++;
+		count++;
+	}
+	count = 0;
+	dup = malloc(sizeof(char) * (length + 1));
+	while (count < (length + 1))
+	{
+		dup[count] = str[count];
+		count++;
+	}
+	dup[count] = '\0';
+	if (str == NULL)
+		return (NULL);
+	return (dup);
+}
