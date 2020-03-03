b8c1105da7fa52a4f54e68891e57303db98bf80f
MiguelCF06 <1480@holbertonschool.com>
2020-03-03 18:47:00 +0000

    Fix: Task 2 Malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,25 +18,31 @@ char *str_concat(char *s1, char *s2)
 		s1 = malloc(sizeof(char));
 		*s1 = '\0';
 	}
+
 	if (s2 == NULL)
 	{
 		s2 = malloc(sizeof(char));
 		*s2 = '\0';
 	}
+
 	conar = malloc(sizeof(s1) + sizeof(s2));
+
 	if (conar != NULL)
 	{
-	for (con1 = 0; s1[con1] != '\0'; con1++)
-	{
-		conar[con1] = s1[con1];
-	}
-	for (con2 = 0; s2[con2] != '\0'; con1++, con2++)
-	{
-		conar[con1] = s2[con2];
-	}
-	return (conar);
+		for (con1 = 0; s1[con1] != '\0'; con1++)
+		{
+			conar[con1] = s1[con1];
+		}
+
+		for (con2 = 0; s2[con2] != '\0'; con1++, con2++)
+		{
+			conar[con1] = s2[con2];
+		}
+
+		return (conar);
 	}
 	else
 		return (NULL);
 	return (conar);
+	free(conar);
 }
