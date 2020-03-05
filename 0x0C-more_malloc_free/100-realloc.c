55012bdc2161ae5db6f719c402450819a38fe0e2
MiguelCF06 <1480@holbertonschool.com>
2020-03-05 16:15:20 +0000

    Fixed: Free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _realloc - Reallocate a space of memory
+ * @old_size: Principal size the old one
+ * @new_size: New size of the ptr
+ * @ptr: Pointer
+ * Return: Null or the reallocated pointer.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	if (ptr == NULL)
+		return (NULL);
+	if (new_size > old_size)
+	{
+		free(ptr);
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(sizeof(new_size) * (old_size + new_size));
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		return (NULL);
+		free(ptr);
+	}
+	return (ptr);
+}
