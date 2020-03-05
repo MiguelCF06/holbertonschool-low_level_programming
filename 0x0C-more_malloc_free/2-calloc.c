3630ed10e0d156b66cde5e2b9726146cc6c7d401
MiguelCF06 <1480@holbertonschool.com>
2020-03-05 17:33:59 +0000

    Fixed: Added a loop and change malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,18 +2,25 @@
 #include <stdlib.h>
 /**
  * _calloc - Allocate memory for an array.
- * @nmemb: Num memory bytes
- * @size: Size
+ * @nmemb: Num memory bytes of array
+ * @size: Size of char
  * Return: Null or the allocate memory of the array.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int *allocmem = NULL;
+	unsigned int x;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
-	allocmem = malloc(sizeof(unsigned int) * (nmemb * size));
-
-		return (allocmem);
+	allocmem = malloc(sizeof(unsigned int) * nmemb);
+	if (allocmem == NULL)
+	{
+		return (NULL);
+	}
+	for (x = 0; x < nmemb; x++)
+	{
+		allocmem[x] = 0;
+	}
+	return (allocmem);
 }
