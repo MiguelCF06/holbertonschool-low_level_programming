3368218ffcd3a591b81c898ef391ee4a9a92ac55
MiguelCF06 <1480@holbertonschool.com>
2020-03-05 17:26:49 +0000

    Fixed: 3rd If

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,28 +10,28 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-
-	if (ptr == NULL)
-		return (NULL);
 	if (new_size > old_size)
 	{
 		free(ptr);
 		ptr = malloc(new_size);
 		return (ptr);
 	}
-	if (new_size == old_size)
+	else
 	{
-		return (ptr);
-	}
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
-		return (ptr);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		return (NULL);
-		free(ptr);
+		if (new_size == old_size)
+		{
+			return (ptr);
+		}
+		if (ptr == NULL)
+		{
+			ptr = malloc(new_size);
+			return (ptr);
+		}
+		if (new_size == 0 && ptr != NULL)
+		{
+			free(ptr);
+			return (NULL);
+		}
 	}
 	return (ptr);
 }
