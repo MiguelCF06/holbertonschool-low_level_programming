eb660b50d4da7d3b50ae113684a8a4fa5ce1d906
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-10 02:38:50 +0000

    First Update

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define x "Hello, Holberton\n"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void main(void)
+{
+	printf("%s", x);
+}
