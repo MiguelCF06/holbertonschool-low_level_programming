4c3715876c4d71483339312f1247cc1e003415e4
MiguelCF06 <1480@holbertonschool.com>
2020-03-11 18:50:46 +0000

    Fixed: the conditional

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - Adds numbers
+ * @a: First integer
+ * @b: Second integer
+ * Return: The two numbers added
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Difference of numbers
+ * @a: First integer
+ * @b: Second integer
+ * Return: The difference of the numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Multiplies numbers
+ * @a: First integer
+ * @b: Second integer
+ * Return: The two numbers multiplies
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Divides numbers
+ * @a: First integer
+ * @b: Second integer
+ * Return: The division of the numbers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Remainder of the division of integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: The result of the remainder of the division of integers
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
