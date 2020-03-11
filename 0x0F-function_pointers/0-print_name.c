0aea307ab5b6b5e11acac8679ab61bd0d35fe914
MiguelCF06 <1480@holbertonschool.com>
2020-03-11 13:51:13 +0000

    Added: Conditional

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - Prints a name
+ * @name: Name
+ * @f: Function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	f(name);
+}
