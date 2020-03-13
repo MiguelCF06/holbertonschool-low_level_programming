813a30a9922d26cc2376cdba9c003751b1b38b28
MiguelCF06 <1480@holbertonschool.com>
2020-03-13 14:49:14 +0000

    First Update

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_numbers - function that returns the sum of all its parameters
+ * @n: Num of parameters
+ * @separator: The comma that is a string
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nu;
+	unsigned int x;
+	int p = 0;
+
+	if (separator == NULL)
+		return;
+
+	va_start(nu, n);
+	for (x = 0; x < n; x++)
+	{
+		p = va_arg(nu, unsigned int);
+		printf("%i", p);
+		if (x < n - 1)
+		{
+			`printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(nu);
+}
