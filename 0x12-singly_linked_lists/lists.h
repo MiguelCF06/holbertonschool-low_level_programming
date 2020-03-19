6c24cd60bea54b853cc5092a8db72e77e0d998c4
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-19 18:53:32 +0000

    Added prototype of the advanced task

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -25,5 +26,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
+void printfirst(void)__attribute__ ((constructor));
 #endif
