2604c6eb2ff5e5eca91bc12a6de4a6f47820f976
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-19 18:53:04 +0000

    Divided the printf in two 'cause betty

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ *printfirst - Print first before the main is exexuted
+ */
+void printfirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
