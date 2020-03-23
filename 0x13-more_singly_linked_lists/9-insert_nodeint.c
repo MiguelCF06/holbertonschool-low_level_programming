de683d5b65a6704d3a05ec24ae3fa5a5b90b92c9
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-23 17:19:46 +0000

    Added conditional for return NULL

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+ *insert_nodeint_at_index -  Inserts a new node at a given position
+ *@head: pointer to the list. Nodes
+ *@idx: Position in the linked list
+ *@n: Data to put in the index.
+ *Return:  the address of the new node, or NULL if it failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp, *prev, *current;
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!*head && idx != 0)
+		return (NULL);
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+
+	current = *head;
+	temp = current;
+
+	if (idx == 0)
+	{
+		new_node->next = current;
+		current = new_node;
+		return (current);
+	}
+	else
+	{
+		while (idx--)
+		{
+			prev = temp;
+			temp = temp->next;
+		}
+		prev->next = new_node;
+		new_node->next = temp;
+		return (current);
+	}
+}
