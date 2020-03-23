cb1496dee9791067b831f1e1b6931e925edcd0c5
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-23 15:26:05 +0000

    First Update

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *free_listint2 - Frees a linked list (list_t)
+ *@head: pointer to pointer in the list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *current;
+
+	current = malloc(sizeof(listint_t));
+	if (*head == NULL || *head == NULL)
+	{
+		return;
+	}
+	current = *head;
+	while (current != NULL &&  head != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
+	*head = NULL;
+}
