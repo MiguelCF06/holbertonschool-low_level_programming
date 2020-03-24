610cdfb1ea9021501e764e1ac994d876b224f621
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-24 16:22:40 +0000

    Fixed signs

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,8 +11,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp, *prev, *current;
 	listint_t *new_node = malloc(sizeof(listint_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
 	if (*head == NULL && idx != 0)
 		return (NULL);
+
 	new_node->n = n;
 
 	current = *head;
