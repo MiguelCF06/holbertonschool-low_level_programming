7655631f9ad6f7f74656282ba44bbffd168168dd
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-26 17:47:46 +0000

    Added conditional for return -1 if an error ocurred

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,6 +9,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int val;
 
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
 	val = ((n >> index) & 1);
 
 	return (val);
