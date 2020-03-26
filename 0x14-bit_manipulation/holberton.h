3fee981d09eb3edb5c444250fcd91db002d02dab
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-26 17:27:06 +0000

    Added prototypes

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLB_H
+#define _HOLB_H
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+#endif
