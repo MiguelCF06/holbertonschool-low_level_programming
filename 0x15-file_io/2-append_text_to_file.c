fe8bc315f64ad066e18c1a9962f88da959b65b96
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-31 18:31:24 +0000

    First Update

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ *lenContent - Looks for the length of the text_content
+ *@ctxt: is the pointer to the text_content
+ *Return: The length of the string
+ */
+int lenContent(char *ctxt)
+{
+	int i;
+
+	for (i = 0; ctxt[i]; i++)
+	{
+		;
+	}
+	return (i);
+}
+/**
+ *append_text_to_file - append text to the end of a file.
+ *@filename: is the name of the file
+ *@text_content: The content of the file
+ *Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_APPEND | O_WRONLY);
+
+	if (text_content != NULL)
+		write(fd, text_content, lenContent(text_content));
+	close(fd);
+
+	return (1);
+}
