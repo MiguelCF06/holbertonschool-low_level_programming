61b82093607674d2bd2d052c91239ce6430779ef
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-31 20:50:39 +0000

    Added prototypes

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -11,5 +11,9 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int lenContent(char *ctxt);
+void numArg(int args);
+void existence(char *file, int fd_from, int fd_to, ssize_t err);
+void notCreate(char *file, int fd_from, int fd_to, ssize_t err);
+void noClose(int lookClose, int fd);
 
 #endif
