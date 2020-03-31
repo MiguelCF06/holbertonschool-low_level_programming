663bef41398356b918b918aaafb9d202d670da8f
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-03-31 17:33:12 +0000

    Fixed the write section

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,21 +23,18 @@ int lenContent(char *ctxt)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int lenCtxt;
 	ssize_t lW = 0;
-
-	lenCtxt = lenContent(text_content);
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
 
 	if (text_content != NULL)
-		lW = write(fd, text_content, lenCtxt);
+		lW = write(fd, text_content, lenContent(text_content));
 	close(fd);
 	if (lW == -1)
 		return (-1);
