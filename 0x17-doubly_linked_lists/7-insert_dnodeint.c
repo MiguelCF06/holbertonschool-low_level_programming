a3bc97395ffae7046d0b26cd2e6c2e1707da5965
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-02 17:13:48 +0000

    [Updated]Added more conditionals, and removed frees

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,43 +9,38 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
-	dlistint_t *new_node, *tmp, *tmp2;
+	dlistint_t *new_node, *tmpCur, *tmp2;
 
-	if (h == NULL && idx != 0)
+	if (h == NULL)
 		return (NULL);
+
+	if (idx != 0)
+	{
+		tmpCur = *h;
+		for (i = 0; i < idx - 1 && tmpCur != NULL; i++)
+			tmpCur = tmpCur->next;
+		if (tmpCur == NULL)
+			return (NULL);
+	}
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	tmp = *h;
+
 	if (idx == 0)
 	{
-		if (tmp != NULL)
-		{
-			new_node->next = tmp;
-			tmp->prev = new_node;
-		}
-		else
-			new_node->next = NULL;
-		new_node->prev = NULL;
+		tmp2 = *h;
 		*h = new_node;
-		return (*h);
+		new_node->prev = NULL;
 	}
-	for (i = 0; i < idx - 1 && tmp != NULL; i++)
+	else
 	{
-		if (tmp == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		tmp = tmp->next;
+		new_node->prev = tmpCur;
+		tmp2 = tmpCur->next;
+		tmpCur->next = new_node;
 	}
-	if (tmp == NULL)
-		free(new_node);
-	tmp2 = tmp;
-	tmp = tmp->next;
-	tmp2->next = new_node;
-	new_node->next = tmp;
-	new_node->prev = tmp2;
+	new_node->next = tmp2;
+	if (new_node->next == NULL)
+		new_node->next->prev = new_node;
 	return (new_node);
 }
