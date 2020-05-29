e370f967ff7d330fef6042b55fd9b6fbb2fa6885
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-05-29 15:57:22 +0000

    [Fixed]Some prototypes

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -35,9 +35,9 @@ typedef struct hash_table_s
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-void node_handler(hash_table_t *ht, hash_node_t *node);
+hash_node_t *node_handler(const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
