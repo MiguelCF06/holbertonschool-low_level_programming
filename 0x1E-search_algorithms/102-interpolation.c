4cdc9e89fbd16fefdd0d00c06a94d31ddf7f0ed8
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-08-26 23:36:49 +0000

    [Updated]Fixed a warning, removing an else in the line 35

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -32,11 +32,9 @@ int interpolation_search(int *array, size_t size, int value)
 	}
 	if (value == array[low])
 		return (low);
-	else
-	{
-		pos = low + (((double)(high - low) / (array[high] - array[low]))
-			     * (value - array[low]));
-		printf("Value checked array[%lu] is out of range\n", pos);
-		return (-1);
-	}
+
+	pos = low + (((double)(high - low) / (array[high] - array[low]))
+		     * (value - array[low]));
+	printf("Value checked array[%lu] is out of range\n", pos);
+	return (-1);
 }
